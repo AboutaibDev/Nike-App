eec3d3e6f15143ac5544f55227c08b96c9de8a90
KinGuzy3 <ziglass2020@gmail.com>
2024-11-08 21:54:57 +0100

    fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,13 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter_test/flutter_test.dart';
 import 'package:eco_app/main.dart';
 
 void main() {
-testWidgets('Welcome page has welcome text', (WidgetTester tester) async {
-  await tester.pumpWidget(const MyApp());
+  // Test 1: Verify that WelcomePage is displayed
+  testWidgets('WelcomePage is displayed', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
 
-  // Verify that the welcome page contains "Welcome" text.
-  expect(find.text('Welcome'), findsOneWidget);
-});
+    // Check if Welcome text is displayed
+    expect(find.text('Just Do It'), findsOneWidget);
+  });
 
 }
